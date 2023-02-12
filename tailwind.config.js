ee10c16c9068f066b1b0fe69ccdbda3448fb6950
Rohit Kumar Singh <Rksingh620@gmail.com>
2023-02-12 14:10:08 +0530

    initial commit

---
+++ tailwind.config.js
@@ -0,0 +1,14 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: ['./src/**/*.{ts,js,jsx,tsx}'],
+  theme: {
+    extend: {
+      colors: {
+        primary: '#0B0F17',
+        tertiary: '#101827',
+        secondary: "#F59E0C"
+      }
+    },
+  },
+  plugins: [],
+}
